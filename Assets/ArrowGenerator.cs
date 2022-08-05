a32491d54e2f25317e9dafce7cb8db6ad5410f83
ltbur <ltburtonst@gmail.com>
2022-08-05 14:25:04 +0900

    作成

---
+++ Assets/ArrowGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowGenerator : MonoBehaviour
+{
+    public GameObject prefab;
+    float span=1.0f;
+    float delta=0;
+
+    IEnumerator ArrowGenerate(){
+        while(true){
+            yield return new WaitForSeconds(1f);
+            Instantiate(
+                prefab,
+                new Vector3(Random.Range(-6f,6f),7f,0),
+                Quaternion.identity
+                );
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.delta+=Time.deltaTime;
+        if(this.delta > this.span){
+            this.delta=0;
+            GameObject obj=Instantiate(prefab);
+            
+        }
+    }
+}
---
+++ Assets/GameDirector.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameDirector : MonoBehaviour
+{
+    public Image hpGage;
+
+    public void DecreaseHp(){
+        hpGage.fillAmount -= 0.1f;
+    }
+}
